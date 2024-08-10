f92abd0fa23de7c9f342f7d49eefbde02f9c0ae2
Harkirat Singh <84682078+harkiratsingh2002@users.noreply.github.com>
2024-08-10 21:59:12 +0930

    live chat integrated

--- cars-dealing-frontend-master/src/components/Navbar.jsx
+++ cars-dealing-frontend-master/src/components/Navbar.jsx
@@ -413,6 +413,7 @@ export default function Navbar() {
         <Button sx={NavLinkStyles}>Login</Button>
         <Button sx={NavLinkStyles}>Sign Up</Button> */}
       </div>
+
     </>
   );
 
@@ -477,6 +478,40 @@ export default function Navbar() {
       <Outlet />
 
       <Footer />
+            {/* <!--Use the below code snippet to provide real time updates to the live chat plugin without the need of copying and paste each time to your website when changes are made via PBX--> */}
+<call-us-selector phonesystem-url="https://1676.3cx.cloud" party="japandirectautos"></call-us-selector>
+ 
+ {/* <!--Incase you don't want real time updates to the live chat plugin when options are changed, use the below code snippet. Please note that each time you change the settings you will need to copy and paste the snippet code to your website--> 
+ <!--<call-us 
+ phonesystem-url="https://1676.3cx.cloud" 
+ style="position:fixed;font-size:16px;line-height:17px;z-index: 99999;right: 20px; bottom: 20px;" 
+ id="wp-live-chat-by-3CX" 
+ minimized="true" 
+ animation-style="noanimation" 
+ party="japandirectautos" 
+ minimized-style="bubbleright" 
+ allow-call="false" 
+ allow-video="false" 
+ allow-soundnotifications="true" 
+ enable-mute="true" 
+ enable-onmobile="true" 
+ offline-enabled="true" 
+ enable="true" 
+ ignore-queueownership="false" 
+ authentication="both" 
+ show-operator-actual-name="true" 
+ aknowledge-received="true" 
+ gdpr-enabled="false" 
+ message-userinfo-format="both" 
+ message-dateformat="both" 
+ lang="browser" 
+ button-icon-type="default" 
+ greeting-visibility="none" 
+ greeting-offline-visibility="none" 
+ chat-delay="2000" 
+ enable-direct-call="false" 
+ enable-ga="false" 
+ ></call-us>-->  */}
     </>
   );
 }
